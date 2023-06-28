d30c8f2531a04d24880c9264d9e3f47ed6b2dd04
eachann <each1024@qq.xn--com-xma>
2023-06-28 15:39:54 +0800

    chore - 更新依赖 - 更新版权/标题信息

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@ import { invoke } from '@tauri-apps/api'
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'TauriApp',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: 'An app that is not defined' },
     {
       name: 'theme-color',
       content: () => isDark.value ? '#00aba9' : '#ffffff',
