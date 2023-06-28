8c7f62bc28bedc89aab8971025dd98c98f957b3a
eachann <each1024@qq.xn--com-xma>
2023-06-28 16:59:35 +0800

    fix bug

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { invoke } from '@tauri-apps/api'
-
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
@@ -22,15 +20,9 @@ useHead({
   ],
 })
 
-
-
-// now we can call our Command!
-// Right-click the application background and open the developer tools.
-// You will see "Hello, World!" printed in the console!
-invoke('greet', { name: 'World' })
-  // `invoke` returns a Promise
-  .then((response) => console.log(response))
 </script>
+
+
 
 <template>
   <RouterView />
